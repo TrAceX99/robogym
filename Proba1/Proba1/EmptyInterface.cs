6a3b96fea8b788d440fb3a5bb4dd4f7d31c6c53c
NikolaDjokic9 <djokicnikola999@gmail.com>
2017-04-06 19:24:34 +0200

    proba

---
+++ Proba1/Proba1/EmptyInterface.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using MonoBrickFirmware;
+using MonoBrickFirmware.Movement;
+using MonoBrickFirmware.Display;
+using MonoBrickFirmware.Sensors;
+using MonoBrickFirmware.UserInput;
+using System.Threading;
+namespace Asdf
+{
+	class MainClass
+	{
+		public static void Scan()
+		{
+			MotorSync m = new MotorSync(MotorPort.OutB, MotorPort.OutD);
+			WaitHandle wh;
+			while (true)
+			{
+				wh = m.StepSync(50, -200, 300, true);
+				wh.WaitOne();
+				wh = m.StepSync(50, 200, 600, true);
+				wh.WaitOne();
+				wh = m.StepSync(50, -200, 300, true);
+				wh.WaitOne();
+				wh = m.StepSync(100, 0, 500, true);
+				wh.WaitOne();
+			}
+		}
+
+		public static void Main(string[] args)
+		{
+			ButtonEvents buts = new ButtonEvents();
+			bool run = true;
+
+			buts.EscapePressed += () =>
+			{
+				run = false;
+			};
+
+			MotorSync motori = new MotorSync(MotorPort.OutB, MotorPort.OutD);
+			EV3IRSensor ir = new EV3IRSensor(SensorPort.In3, IRMode.Proximity);
+
+			Thread bg = new Thread(Scan);
+			bg.Start();
+			while (run && ir.Read() > 80)
+			{
+				LcdConsole.WriteLine(ir.Read().ToString());
+				Thread.Sleep(200);
+			}
+			bg.Abort();
+			motori.Brake();
+			motori.Off();
+		}
+	}
+}

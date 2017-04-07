0465dce141f595e4b51142669a5d258735013d3b
TrAceX <djole1271952@gmail.com>
2017-04-07 13:09:25 +0200

    Dodate konstante, sredjen kod Mnogo public statica

--- Mindstorms/Bobot/TableCleaner.cs
+++ Mindstorms/Bobot/TableCleaner.cs
@@ -10,19 +10,33 @@ namespace TableCleaner
 {
 	class MainClass
 	{
-		public static void Scan()
+		public static class C // Konstante
 		{
-			MotorSync m = new MotorSync(MotorPort.OutB, MotorPort.OutD);
+			public const int scanTurnSpeed = 50;
+			public const int scanForwardSpeed = 100;
+			public const int scanTurnSteps = 300;
+			public const int scanForwardSteps = 600;
+			public const int irScanTreshold = 80;
+			public const int left = -200;
+			public const int right = 200;
+		};
+			
+		static MotorSync motors = new MotorSync(MotorPort.OutB, MotorPort.OutD);
+		static EV3IRSensor ir = new EV3IRSensor(SensorPort.In3, IRMode.Proximity);
+		static bool run = true;
+
+		static void Scan()
+		{
 			WaitHandle wh;
 			while (true)
 			{
-				wh = m.StepSync(50, -200, 300, true);
+				wh = motors.StepSync(C.scanTurnSpeed, C.left, C.scanTurnSteps, true);
 				wh.WaitOne();
-				wh = m.StepSync(50, 200, 600, true);
+				wh = motors.StepSync(C.scanTurnSpeed, C.right, C.scanTurnSteps * 2, true);
 				wh.WaitOne();
-				wh = m.StepSync(50, -200, 300, true);
+				wh = motors.StepSync(C.scanTurnSpeed, C.left, C.scanTurnSteps, true);
 				wh.WaitOne();
-				wh = m.StepSync(100, 0, 500, true);
+				wh = motors.StepSync(C.scanForwardSpeed, 0, C.scanForwardSteps, true);
 				wh.WaitOne();
 			}
 		}
@@ -30,27 +44,22 @@ namespace TableCleaner
 		public static void Main(string[] args)
 		{
 			ButtonEvents buts = new ButtonEvents();
-			bool run = true;
-
 			buts.EscapePressed += () =>
 			{
 				run = false;
 			};
 
-			MotorSync motori = new MotorSync(MotorPort.OutB, MotorPort.OutD);
-			EV3IRSensor ir = new EV3IRSensor(SensorPort.In3, IRMode.Proximity);
-
 			Thread bg = new Thread(Scan);
 			bg.Start();
-			while (run && ir.Read() > 80)
+			while (run && ir.Read() > C.irScanTreshold)
 			{
 				LcdConsole.WriteLine(ir.Read().ToString());
 				Thread.Sleep(200);
 			}
 			bg.Abort();
-			motori.Brake();
+			motors.Brake();
 			Thread.Sleep (100);
-			motori.Off();
+			motors.Off();
 		}
 	}
 }

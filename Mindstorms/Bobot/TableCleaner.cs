7090e299090e7ae916324b87075880e3109c6b51
TrAceX <djole1271952@gmail.com>
2017-04-08 16:16:49 +0200

    Zavrsen osnovni algoritam Dodato vracanje na pocetnu rotaciju u Dropoff(), printanje na LCD, ispravljen poneki bag.

--- Mindstorms/Bobot/TableCleaner.cs
+++ Mindstorms/Bobot/TableCleaner.cs
@@ -4,12 +4,12 @@ using MonoBrickFirmware.Movement;
 using MonoBrickFirmware.Display;
 using MonoBrickFirmware.Sensors;
 using MonoBrickFirmware.UserInput;
-using System.Threading;
 using MonoBrickFirmware.Display.Dialogs;
+using System.Threading;
 
 namespace TableCleaner
 {
-	class MainClass
+	class Bobot
 	{
 		public static class C // Konstante
 		{
@@ -36,6 +36,7 @@ namespace TableCleaner
 			public const uint armLiftedPos = 100; // Potrebno podesavanje
 			public const sbyte armSpeed = 100;
 
+			public const int motorBrakeSleepTime = 200;
 			public const uint oneeightyInSteps = 800;
 			public const short left = -200;
 			public const short right = 200;
@@ -51,6 +52,8 @@ namespace TableCleaner
 
 		static MotorSync motors = new MotorSync(MotorPort.OutB, MotorPort.OutD);
 		static Motor armMotor = new Motor(MotorPort.OutA);
+		static Motor lMotor = new Motor(MotorPort.OutB);
+		static Motor rMotor = new Motor(MotorPort.OutD);
 		static EV3IRSensor ir = new EV3IRSensor(SensorPort.In3, IRMode.Proximity);
 		static EV3ColorSensor color = new EV3ColorSensor(SensorPort.In4);
 		static WaitHandle motorWH;
@@ -63,18 +66,23 @@ namespace TableCleaner
 		// Resetuje ruku na polozaj 0 (Open)
 		static void ArmCalibrate()
 		{
+			LcdConsole.WriteLine("Kalibracija ruke...");
 			armMotor.ResetTacho();
-			armMotor.SetPower(C.armCalibrationPower);
+
+			armMotor.SetPower(-C.armCalibrationPower);
 			Thread.Sleep(C.armCalibrationTime);
 			armMotor.Off();
+
 			Thread.Sleep(500);
 			armMotor.ResetTacho();
 			armPos = ArmPosition.Open;
+			LcdConsole.WriteLine("Kalibracija završena");
 		}
 
 		// Pomera ruku na odredjeni polozaj
 		static void ArmMove(ArmPosition targetPos)
 		{
+			LcdConsole.WriteLine("Pomeranje ruke na položaj {0}", targetPos.ToString());
 			int armTacho = armMotor.GetTachoCount();
 			int direction = Math.Sign((uint)targetPos - armTacho);
 			uint steps = (uint)Math.Abs((uint)targetPos - armTacho);
@@ -85,6 +93,7 @@ namespace TableCleaner
 			armPos = targetPos;
 			if (armPos != ArmPosition.Lifted)
 				armMotor.Off();
+			LcdConsole.WriteLine("Pozicija motora ruke: {0}", armMotor.GetTachoCount().ToString());
 		}
 
 		// Kretanje Bobota prilikom trazenja predmeta
@@ -105,6 +114,9 @@ namespace TableCleaner
 		// Algoritam trazenja
 		static bool Search()
 		{
+			LcdConsole.WriteLine("Trazenje predmeta...");
+			lMotor.ResetTacho(); // Resetuje tacho motora zbog okretanja na kraju
+			rMotor.ResetTacho(); 
 			Thread movement = new Thread(SearchMove);
 			movement.IsBackground = true; // Background thread se gasi kada se main zavrsi
 			movement.Start();
@@ -113,12 +125,17 @@ namespace TableCleaner
 			while (ir.Read() > C.irScanTreshold && progress < tableLenght) {
 				LcdConsole.WriteLine(ir.Read().ToString());
 				if (color.Read() == 0)
-					throw new Exception("Color sensor value is zero");
+				{
+					movement.Abort();
+					motors.Brake();
+					throw new Exception("Vrednost senzora za boju je 0!");
+				}
 				Thread.Sleep(200);
 			}
 
 			movement.Abort();
 			motors.Brake();
+			Thread.Sleep(C.motorBrakeSleepTime);
 			if (progress < tableLenght)
 				return false;
 			else
@@ -127,6 +144,7 @@ namespace TableCleaner
 		// Algoritam podizanja predmeta
 		static void Pickup()
 		{
+			LcdConsole.WriteLine("Predmet pronadjen");
 			ArmMove(ArmPosition.Open);
 			motorWH = motors.StepSync(C.pickupForwardSpeed, 0, (uint)(ir.Read() * C.stepsPerCm), true);
 			motorWH.WaitOne();
@@ -135,6 +153,7 @@ namespace TableCleaner
 
 		static void Carry()
 		{
+			LcdConsole.WriteLine("Vracanje do IR beacona...");
 			ir.Mode = IRMode.Seek;
 			BeaconLocation beacon = ir.ReadBeaconLocation();
 
@@ -145,19 +164,33 @@ namespace TableCleaner
 			}
 			while (beacon.Distance > C.carryDistanceToBeacon) {
 				motors.SetSpeed(C.carryForwardSpeed, beacon.Location); // Potreban koeficijent
+				beacon = ir.ReadBeaconLocation(); // Mozda potrebno Thread.Sleep
 			}
 			motors.Brake();
-			Thread.Sleep(200);
+			Thread.Sleep(C.motorBrakeSleepTime);
+			LcdConsole.WriteLine("Rastojanje do IR beacona: {0}", ir.ReadBeaconLocation().Distance.ToString());
 		}
 
 		static void Dropoff()
 		{
+			LcdConsole.WriteLine("Spustanje predmeta...");
 			ArmMove(ArmPosition.Open);
+
 			motorWH = motors.StepSync(C.dropoffPullbackSpeed, 0, C.dropoffPullbackSteps, true);
 			motorWH.WaitOne();
 			ArmMove(ArmPosition.Closed);
-			motorWH = motors.StepSync(C.dropoffTurnSpeed, C.right, C.oneeightyInSteps, true);
+
+			LcdConsole.WriteLine("Tacho levog motora: {0}", lMotor.GetTachoCount().ToString());
+			LcdConsole.WriteLine("Tacho desnog motora: {0}", rMotor.GetTachoCount().ToString());
+			// Ovaj deo treba da vrati Bobota na pocetnu rotaciju
+			// Vrlo verovatno ne radi
+			int deltaTacho = rMotor.GetTachoCount() - lMotor.GetTachoCount();
+			sbyte speed = (sbyte)(C.dropoffTurnSpeed * Math.Sign(deltaTacho));
+			uint steps = (uint)(C.oneeightyInSteps + deltaTacho);
+			motorWH = motors.StepSync(speed, C.right, steps, true);
 			motorWH.WaitOne();
+			Thread.Sleep(C.motorBrakeSleepTime);
+			LcdConsole.WriteLine("Ciklus zavrsen!");
 		}
 
 		public static void Main(string[] args)
@@ -165,7 +198,7 @@ namespace TableCleaner
 			try {
 				ButtonEvents buts = new ButtonEvents();
 				buts.EscapePressed += () => {
-					throw new Exception("Program terminated by escape button");
+					throw new Exception("Escape gumb je stisnut!");
 				};
 
 				ArmCalibrate ();
@@ -178,6 +211,7 @@ namespace TableCleaner
 						Carry ();
 						Dropoff ();
 					} else {
+						LcdConsole.WriteLine("Ceo sto je ociscen!");
 						Carry ();
 						break;
 					}
@@ -185,10 +219,11 @@ namespace TableCleaner
 			} catch (Exception ex) {
 				InfoDialog d = new InfoDialog(ex.Message, "Exception");
 				d.Show();
+				Thread.Sleep(2000);
 			} finally {
 				motors.Off();
 				armMotor.Off();
-				Thread.Sleep(1000);
+				Thread.Sleep(500);
 			}
 		}
 	}

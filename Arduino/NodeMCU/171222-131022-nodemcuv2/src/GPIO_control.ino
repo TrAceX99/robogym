202716db0a3717afec393db599d128ae00efe39f
TrAceX <djole1271952@gmail.com>
2017-12-24 16:16:18 +0100

    wifi connect linkovi

--- Arduino/NodeMCU/171222-131022-nodemcuv2/src/GPIO_control.ino
+++ Arduino/NodeMCU/171222-131022-nodemcuv2/src/GPIO_control.ino
@@ -38,47 +38,47 @@ void handleRoot() {
 			Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
 			server.sendHeader("Location", "/menu", true);
 			server.send(307, "text/plain", "");
-			Serial.println("Redirected\n");
+			Serial.println("Redirected to menu\n");
 		}
 }
 
-void handleRedirect() {	
-    http.begin("http://" + dataserverIP + "/menu.html");
-    int httpCode = http.GET();
-    
-    if (httpCode > 0) {
-        Serial.println("GET request to menu.html ok\n");
-        String html = http.getString();
-        server.send(200, "text/html", html);
-    } else {
-        Serial.printf("[REDIRECT] GET failed, error: %s\n", http.errorToString(httpCode).c_str());
-        Serial.println("Opening file from flash");
-        File f = SPIFFS.open("/menu.html", "r");
-        if (!f) Serial.println("File open failed!");
-        String html = f.readString();
-        server.send(200, "text/html", html);
-        f.close();
-    }
-    Serial.println();
+void handleMenu() {	
+	http.begin("http://" + dataserverIP + "/menu.html");
+	int httpCode = http.GET();
+	
+	if (httpCode > 0) {
+		Serial.println("GET request to menu.html ok\n");
+		String html = http.getString();
+		server.send(200, "text/html", html);
+	} else {
+		Serial.printf("[REDIRECT] GET failed, error: %s\n", http.errorToString(httpCode).c_str());
+		Serial.println("Opening file from flash");
+		File f = SPIFFS.open("/menu.html", "r");
+		if (!f) Serial.println("File open failed!");
+		String html = f.readString();
+		server.send(200, "text/html", html);
+		f.close();
+	}
+	Serial.println();
 }
 
 void handleOffline() {	
-    Serial.println("Offline mode started");
-    File f = SPIFFS.open("/offline.html", "r");
-    if (!f) Serial.println("File open failed!");
-    String html = f.readString();
-    server.send(200, "text/html", html);
-    f.close();
+	Serial.println("Offline mode started");
+	File f = SPIFFS.open("/offline.html", "r");
+	if (!f) Serial.println("File open failed!");
+	String html = f.readString();
+	server.send(200, "text/html", html);
+	f.close();
 }
 
 void handleSubmit() {	
-    for (uint8_t i = 0; i < server.args(); i++) {
-        int pin = server.argName(i).toInt();
-        int state = server.arg(i).toInt();
-        digitalWrite(pin, state);
-        Serial.println("Set pin " + String(pin) + " to " + String(state));
-    }
-    server.send(200, "text/plain", "Ako vidis ovo svaka cast");
+	for (uint8_t i = 0; i < server.args(); i++) {
+		int pin = server.argName(i).toInt();
+		int state = server.arg(i).toInt();
+		digitalWrite(pin, state);
+		Serial.println("Set pin " + String(pin) + " to " + String(state));
+	}
+	server.send(200, "text/plain", "Ako vidis ovo svaka cast");
 }
 
 void testSubmit() {
@@ -91,81 +91,96 @@ void testSubmit() {
 }
 
 void handleConnect() {	
-    if (WiFi.localIP().toString() != "0.0.0.0") {
-        if (server.hasArg("ssid") && server.hasArg("pass") && server.arg("ssid") != "") {
-        ssid = server.arg("ssid");
-        pass = server.arg("pass");
-        connectWiFi();
-        } else {
-        Serial.println("Invalid arguments!\n");
-        }
-    }
+	if (WiFi.localIP().toString() == "0.0.0.0") {
+		if (server.hasArg("ssid") && server.hasArg("pass") && server.arg("ssid") != "") {
+			ssid = server.arg("ssid");
+			pass = server.arg("pass");
+			Serial.printf("WiFi ssid:%s, pass:%s; attempting to connect...", ssid.c_str(), pass.c_str());
+			if (connectWiFi()) server.send(200, "text/plain", "Connected");
+			else server.send(200, "text/plain", "Failed");
+			return;
+		} else {
+			Serial.println("Invalid arguments!\n");
+			server.send(200, "text/plain", "Invalid");
+		}
+	} else {
+		Serial.printf("Already connected\nSSID:%s\npassword:%s\nIP:%s\n\n",
+		ssid.c_str(), pass.c_str(), WiFi.localIP().toString().c_str());
+	}
 
-    server.sendHeader("Location", "/", true);
-    server.send(307, "text/plain", "");
+	//server.sendHeader("Location", "/", true);
+	server.send(200, "text/plain", "Already connected");
 }
 
 void handleDisconnect() {
-    server.sendHeader("Location", "/", true);
-    server.send(307, "text/plain", "");
-    WiFi.disconnect();
-    Serial.printf("Disconnected from %s\n\n", ssid.c_str());
+	server.sendHeader("Location", "/", true);
+	server.send(307, "text/plain", "");
+	WiFi.disconnect();
+	Serial.printf("Disconnected from %s\n\n", ssid.c_str());
 }
 
 void handleGet() {
-    StaticJsonBuffer<400> buffer;
-    JsonObject &json = buffer.createObject();
+	StaticJsonBuffer<400> buffer;
+	JsonObject &json = buffer.createObject();
 
-    int8_t networks = WiFi.scanNetworks();
+	if (server.arg("request") == "wifiscan") {
+		int8_t networks = WiFi.scanNetworks();
 
-    json["networks"] = networks;
-    WiFi.localIP().toString() != "0.0.0.0" ? json["connected"] = ssid : json["connected"] = (char*)NULL;
-    JsonArray& j_ssid = json.createNestedArray("ssid");
-    JsonArray& j_channel = json.createNestedArray("channel");
-    JsonArray& j_rssi = json.createNestedArray("rssi");
-    JsonArray& j_enc = json.createNestedArray("enc");
-    for (int8_t i = 0; i < networks; i++) {
-        j_ssid.add(WiFi.SSID(i));
-        j_channel.add(WiFi.channel(i));
-        j_rssi.add(WiFi.RSSI(i));
-        WiFi.encryptionType(i) == ENC_TYPE_NONE ? j_enc.add("open") : j_enc.add("");
-    }
+		json["networks"] = networks;
+		WiFi.localIP().toString() != "0.0.0.0" ? json["connected"] = ssid : json["connected"] = (char*)NULL;
+		JsonArray& j_ssid = json.createNestedArray("ssid");
+		JsonArray& j_channel = json.createNestedArray("channel");
+		JsonArray& j_rssi = json.createNestedArray("rssi");
+		JsonArray& j_enc = json.createNestedArray("enc");
+		for (int8_t i = 0; i < networks; i++) {
+			j_ssid.add(WiFi.SSID(i));
+			j_channel.add(WiFi.channel(i));
+			j_rssi.add(WiFi.RSSI(i));
+			WiFi.encryptionType(i) == ENC_TYPE_NONE ? j_enc.add("open") : j_enc.add("");
+		}
+	}
+	if (server.arg("request") == "wifistatus") {
+		String ip = WiFi.localIP().toString();
+		ip == "0.0.0.0" ? json["status"] = false : json["status"] = true;
+		json["ssid"] = ssid;
+		json["ip"] = ip;
+	}
 
-    String stringBuffer;
-    json.printTo(stringBuffer);
+	String stringBuffer;
+	json.printTo(stringBuffer);
 	server.send(200, "application/json; charset=utf-8", stringBuffer);
-    Serial.println(stringBuffer);
+	Serial.printf("JSON: %s\n\n", stringBuffer.c_str());
 }
 
 void handleNotFound() {	
-    String message = "File Not Found 404\n\n";
-    message += "URI: ";
-    message += server.uri();
-    message += "\nMethod: ";
-    message += ( server.method() == HTTP_GET ) ? "GET" : "POST";
-    message += "\nArguments: ";
-    message += server.args();
-    message += "\n";
-    
-    for ( uint8_t i = 0; i < server.args(); i++ ) {
-        message += " " + server.argName ( i ) + ": " + server.arg ( i ) + "\n";
-    }
+	String message = "File Not Found 404\n\n";
+	message += "URI: ";
+	message += server.uri();
+	message += "\nMethod: ";
+	message += ( server.method() == HTTP_GET ) ? "GET" : "POST";
+	message += "\nArguments: ";
+	message += server.args();
+	message += "\n";
+	
+	for ( uint8_t i = 0; i < server.args(); i++ ) {
+		message += " " + server.argName ( i ) + ": " + server.arg ( i ) + "\n";
+	}
 
-    server.send ( 404, "text/plain", message );
+	server.send ( 404, "text/plain", message );
 
 }
 
 bool connectWiFi() {	
-    WiFi.begin(ssid.c_str(), pass.c_str());
-    if (WiFi.waitForConnectResult() == WL_CONNECTED) {
-        Serial.printf("Connected to %s, IP: ", ssid.c_str());
-        Serial.println(WiFi.localIP());
-        Serial.println();
-    return true;
-    } else {
-        Serial.println("Connection failed\n");
-        return false;
-    }
+	WiFi.begin(ssid.c_str(), pass.c_str());
+	if (WiFi.waitForConnectResult() == WL_CONNECTED) {
+		Serial.printf("Connected to %s, IP: ", ssid.c_str());
+		Serial.println(WiFi.localIP());
+		Serial.println();
+	return true;
+	} else {
+		Serial.println("Connection failed\n");
+		return false;
+	}
 }
 
 void setup() {
@@ -190,11 +205,11 @@ void setup() {
 	server.on("/", handleRoot);
 	server.on("/test", testSubmit);
 	server.on("/submit", HTTP_POST, handleSubmit);
-	server.on("/menu", handleRedirect);
+	server.on("/menu", handleMenu);
 	server.on("/offline", handleOffline);
-	server.on("/connect", handleConnect);
+	server.on("/connect", HTTP_POST, handleConnect);
 	server.on("/disconnect", handleDisconnect);
-	server.on("/get", handleGet);
+	server.on("/get", HTTP_POST, handleGet);
 	server.onNotFound(handleNotFound);
 
 	server.begin();

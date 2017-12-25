df814126bdfa117f2dd0a890d54d127fcb18e366
TrAceX99 <djole1271952@gmail.com>
2017-12-25 13:58:02 +0100

    Mordenkainen

--- Arduino/Arsic/Arsic.ino
+++ Arduino/Arsic/Arsic.ino
@@ -1,9 +1,11 @@
 void setup() {
-  // put your setup code here, to run once:
-
+  pinMode (4, OUTPUT);
+  digitalWrite (4,HIGH); 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  delay(500);
+  digitalWrite (4,LOW);
+  delay(500);
+  digitalWrite (4,HIGH);
 }

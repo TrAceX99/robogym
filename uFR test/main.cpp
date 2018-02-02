b73eab2936ddc5f1f039f46748ad38afe85c5cd6
TrAceX <djole1271952@gmail.com>
2018-02-02 15:46:38 +0100

    sredjen kod

--- uFR test/main.cpp
+++ uFR test/main.cpp
@@ -22,45 +22,53 @@ void WaitForCard() {
 }
 
 main () {
-    uint8_t* data;
     const uint8_t allow = 0xFF;
-    uint16_t dalMeZezasJbt;
+
+    uint8_t* data;
+    uint16_t dalMeZezas;
+    
     if(ReaderOpen() == 0) {
         while(true) {
             cout << "Press any key to continue, W to write access code to card, F to format card, esc to exit" << endl;
             char a = getch();
+
             if (a == ESC) {
                 ReaderClose();
                 return 0;
-            }  
-            else if (a == 'w') {
+            } else if (a == 'w') {
                 if (GetCardId(&cardType, &cardID))
                     WaitForCard();
-                LinearWrite(&allow, 0, 1, &dalMeZezasJbt, MIFARE_AUTHENT1A, 0);
                 ReaderUISignal(3, 0);
+
+                LinearWrite(&allow, 0, 1, &dalMeZezas, MIFARE_AUTHENT1A, 0);
+
                 cout << " Card access granted";
             } else if (a == 'f') {
                 WaitForCard();
                 ReaderUISignal(3, 0);
+
                 data = new byte[MIFARE_LINEAR_CAPACITY];
                 for(int i = 0; i < MIFARE_LINEAR_CAPACITY; i++) data[i] = 0x00;
-                LinearWrite(data, 0, MIFARE_LINEAR_CAPACITY, &dalMeZezasJbt, MIFARE_AUTHENT1A, 0);
+                LinearWrite(data, 0, MIFARE_LINEAR_CAPACITY, &dalMeZezas, MIFARE_AUTHENT1A, 0);
                 delete[] data;
-                cout << " Bytes written: " << dalMeZezasJbt;
+
+                cout << " Bytes written: " << dalMeZezas;
             } else break;
+
             cout << endl;
         }
         while(true) {
+            cout << "Press esc to exit program" << endl;
             WaitForCard();
             if(GetKeyState(VK_ESCAPE) & 0x8000) break;
 
             cout << hex << showbase << uppercase << "\nType: " << (int)cardType << "\nID:" << cardID << endl;
 
             data = new uint8_t[1];
-            uint16_t bytesReturned;
-            LinearRead(data, 0, 1, &bytesReturned, MIFARE_AUTHENT1A, 0);
-            cout << "Bytes returned: " << bytesReturned << endl << "Data: ";
-            for (int i = 0; i < bytesReturned; i++) {
+            LinearRead(data, 0, 1, &dalMeZezas, MIFARE_AUTHENT1A, 0);
+
+            cout << "Bytes returned: " << dalMeZezas << endl << "Data: ";
+            for (int i = 0; i < dalMeZezas; i++) {
                 cout << (int)data[i];
             }
 

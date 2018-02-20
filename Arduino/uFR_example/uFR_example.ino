678d33a77f2f35031fab09eee21c4c51ee2774bd
TrAceX99 <djole1271952@gmail.com>
2018-02-20 15:40:04 +0100

    minor changes

--- Arduino/uFR_example/uFR_example.ino
+++ Arduino/uFR_example/uFR_example.ino
@@ -1,7 +1,7 @@
 #include <LiquidCrystal.h>
 #include <uFR.h>
 
-LiquidCrystal lcd(2, 3, 4, 5, 6, 7); // rs, en, d4, d5, d6, d7
+LiquidCrystal lcd(2, 3, 4, 5, 6, 7); // RS, E, d4, d5, d6, d7
 
 // If this doesn't work, try switching rx and tx
 uFR reader(10, 11, 12); // rx, tx, reset
@@ -22,7 +22,7 @@ void setup() {
   uint8_t type[4];
   lcd.print("Code: ");
   lcd.print(reader.getReaderType(type), HEX);
-  lcd.print("; Type:");
+  lcd.print("  Type:");
   lcd.setCursor(0, 1);
   for (int i = 0; i < 4; i++) {
     lcd.print(type[i], HEX);
@@ -42,9 +42,9 @@ void loop() {
     lcd.print("Card found! UID:");
     lcd.setCursor(0, 1);
     for (int i = 0; i < length; i++) {
-        lcd.print(cardID[i], HEX);
-        lcd.print(" ");
-      }
+      lcd.print(cardID[i], HEX);
+      lcd.print(" ");
+    }
     digitalWrite(LED_BUILTIN, HIGH);
     delay(3000);
     digitalWrite(LED_BUILTIN, LOW);
@@ -55,9 +55,10 @@ void loop() {
     lcd.setCursor(0, 1);
     lcd.print("0x");
     lcd.print(code, HEX);
-    delay(1000);
+    delay(2000);
     lcd.clear();
     lcd.print("Waiting for card");
   }
+  // Reduce delay if response is slow
   delay(200);
 }
